76f3685daed5b0ca9a269bff5b4f26c3ccb80a19
BryanWi <bryanwi2002@gmail.com>
2023-02-07 19:13:27 -0600

    Squashed commit of the following:
    commit 7763579c9829f9a021c4a4ec978a558994e730ca
    Author: BryanWi <bryanwi2002@gmail.com>
    Date:   Tue Feb 7 19:05:51 2023 -0600
    
        Presidentes complaetos
    
        Ya se tiene la info e imágenes de los 66 presidentes
    
    commit 5e2ff02a14403a7f2df203335a0ffd90bd4e2ea6
    Author: BryanWi <bryanwi2002@gmail.com>
    Date:   Tue Feb 7 18:01:43 2023 -0600
    
        sonidos y borrado de comentarios y código no usado
    
    commit 5a714ffd3bc2d30bf1401a872f87787cf2b085fd
    Author: BryanWi <bryanwi2002@gmail.com>
    Date:   Fri Feb 3 14:05:59 2023 -0600
    
        Textura para fondo y cambio a GLES2
    
        GLES3 -> GLES2
        textura en assets/texturas/image.png

--- game/assets/tarjeta.gd
+++ game/assets/tarjeta.gd
@@ -1,48 +1,20 @@
 extends TextureButton
 
-
 signal send_respuesta(respuesta,button_name)
+
 var NOMBRE:String
 
-
-func _ready():
-	pass # Replace with function body.
-	#set_Data("Porfirio Díaz", "diaz_porfirio_2.jpg")
-	#print(list_files_in_directory("res://imagenes/"))
 
 func set_Data(nombre: String, image_path: String):
 	NOMBRE = nombre
 	name = nombre
 	$ColorRect/Label.text = nombre
 	$ColorRect.visible = false
-#	var image = Image.new()
-#	image.load("res://imagenes/" + image_path)
-#	var t = ImageTexture.new()
-#	t.create_from_image(image)
-#	self.texture_normal = t
 	self.texture_normal = load("res://imagenes/" + image_path)
 
 
 func _on_tarjeta_button_down():
 	emit_signal("send_respuesta",NOMBRE,name)
-	
-
-func list_files_in_directory(path):
-	var files = []
-	var dir = Directory.new()
-	dir.open(path)
-	dir.list_dir_begin()
-
-	while true:
-		var file = dir.get_next()
-		if file == "":
-			break
-		elif not file.begins_with("."):
-			files.append(file)
-
-	dir.list_dir_end()
-
-	return files
 
 
 func _on_tarjeta_mouse_entered():
